35391beb42798fb4df0d872d68a4bd885ee6f331
Timothy Russo <russotimothy@live.it>
2023-03-01 18:44:45 +0100

    feat: migrate api file to typescript

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,39 +1,42 @@
 import { BASE_URL } from './constants';
+import { User } from './genericTypes';
 
-const checkResponse = (res) => {
+const checkResponse = <T>(res: Response): Promise<T> => {
   if (res.ok) {
     console.log(
       `URL: ${res.url}
   Status: ${res.statusText}
   Status code: ${res.status}`
     );
-    return res.json();
+    return res.json() as Promise<T>;
   } else {
     return Promise.reject(`Error: ${res.status}`);
   }
 };
 
-export const getAllUsers = () => {
+export const getAllUsers = (): Promise<User[]> => {
   return fetch(`${BASE_URL}`, {
     headers: {
       'Content-Type': 'application/json',
     },
-  }).then(checkResponse);
+  }).then(checkResponse as (res: Response) => Promise<User[]>);
 };
 
-export const createUser = ({
-  firstName,
-  lastName,
-  birthDay,
-  company,
-  startDate,
-  endDate,
-  addressOne,
-  addressTwo,
-  city,
-  province,
-  userId,
-}) => {
+export const createUser = (userData: User): Promise<User> => {
+  const { 
+    firstName,
+    lastName,
+    birthDay,
+    company,
+    startDate,
+    endDate,
+    addressOne,
+    addressTwo,
+    city,
+    province,
+    userId
+  } = userData;
+
   return fetch(`${BASE_URL}`, {
     method: 'POST',
     headers: {
@@ -52,23 +55,25 @@ export const createUser = ({
       province,
       userId,
     }),
-  }).then(checkResponse);
+  }).then(checkResponse) as Promise<User>;
 };
 
-export const updateUser = ({
-  firstName,
-  lastName,
-  birthDay,
-  company,
-  startDate,
-  endDate,
-  addressOne,
-  addressTwo,
-  city,
-  province,
-  userId,
-  id,
-}) => {
+export const updateUser = (userData: User): Promise<User> => {
+  const { 
+    firstName,
+    lastName,
+    birthDay,
+    company,
+    startDate,
+    endDate,
+    addressOne,
+    addressTwo,
+    city,
+    province,
+    userId,
+    id
+  } = userData;
+
   return fetch(`${BASE_URL}/${id}`, {
     method: 'PUT',
     headers: {
@@ -87,14 +92,14 @@ export const updateUser = ({
       province,
       userId,
     }),
-  }).then(checkResponse);
+  }).then(checkResponse) as Promise<User>;
 };
 
-export const deleteUser = ({ id }) => {
+export const deleteUser = ({ id }: { id: number }): Promise<void> => {
   return fetch(`${BASE_URL}/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
     },
-  }).then(checkResponse);
+  }).then(checkResponse) as Promise<void>;
 };
