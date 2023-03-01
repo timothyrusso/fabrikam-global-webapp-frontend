203757743ed24a688763fd3cc8dfe2d9d1b1a2e7
Timothy Russo <russotimothy@live.it>
2023-03-01 16:27:48 +0100

    feat: confirmation modal

---
+++ src/components/confirmation-modal/confirmation-modal.component.tsx
@@ -0,0 +1,33 @@
+import { Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, IconButton } from "@chakra-ui/react"
+import { DeleteIcon } from "@chakra-ui/icons"
+import { FC } from "react";
+
+export type ConfirmationModalComponentProps = {
+    handleDeleteConfirmation: any;
+}
+
+export const ConfirmationModalComponent: FC<ConfirmationModalComponentProps> = ({ handleDeleteConfirmation }) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+      <>
+        <IconButton aria-label="Remove user" icon={<DeleteIcon />} onClick={onOpen} />
+  
+        <Modal isOpen={isOpen} onClose={onClose} isCentered>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Sicuro di voler cancellare?</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+            </ModalBody>
+  
+            <ModalFooter>
+              <Button colorScheme='blue' mr={3} w={50} onClick={handleDeleteConfirmation}>
+                Si
+              </Button>
+              <Button w={50} onClick={onClose}>No</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
