952b7f60c4cfc1e99494a8e23267ae8c10f789f4
Timothy Russo <russotimothy@live.it>
2023-06-29 17:03:30 +0200

    feat: start configuration for redux toolkit

---
+++ src/redux/toolkitStore.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction, configureStore } from '@reduxjs/toolkit';
+import { User } from '../types/generic-types';
+
+type RootState = {
+  users: User[];
+};
+
+const initialState: RootState = {
+  users: [],
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    fetchUsers: (state, action: PayloadAction<User[]>) => {
+      state.users = action.payload;
+    },
+    updateUser: (state, action: PayloadAction<User>) => {
+      const { id } = action.payload;
+      const index = state.users.findIndex((user) => user.id === id);
+      if (index !== -1) {
+        state.users[index] = action.payload;
+      }
+    },
+    deleteUser: (state, action: PayloadAction<{ id: string }>) => {
+      state.users = state.users.filter((user) => user.id !== action.payload.id);
+    },
+  },
+});
+
+export const { fetchUsers, updateUser, deleteUser } = userSlice.actions;
+
+export const toolkitStore = configureStore({
+  reducer: userSlice.reducer,
+});
